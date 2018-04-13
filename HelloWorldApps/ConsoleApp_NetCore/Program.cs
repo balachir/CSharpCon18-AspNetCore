1d4e87085c9b2b916f8847aea1c40241f46a91d8
Balamurugan Chirtsabesan <balach@microsoft.com>
2018-04-13 10:59:17 -0700

    [HelloWorldApps] Created solution with console apps on .NET Framework and .NET Core

---
+++ HelloWorldApps/ConsoleApp_NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world from Console App (.NET Core)");
+        }
+    }
+}
