0b58306aa00257336abb231003db6b5f541bbe21
Balamurugan Chirtsabesan <balach@microsoft.com>
2018-04-13 13:11:31 -0700

    [MvcWebApps] Created solution with MVC apps with Ind Auth on .NET Framework and .NET Core

---
+++ MvcWebApps/MvcApp_NetFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcApp_NetFramework.Startup))]
+namespace MvcApp_NetFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
